acd864de0aa285597d6b7542fde816f1865df6d3
NotifyOne <anton220812@gmail.com>
2019-09-20 14:54:12 +0300

    End 1.3

--- src/Controller/Practic.php
+++ src/Controller/Practic.php
@@ -3,17 +3,50 @@
 namespace Drupal\my_theme\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\Element;
+use Drupal\Component\Utility\Variable;
 
 class Practic extends ControllerBase {
+
   function simple() {
     return [
       '#theme' => 'custom_list',
       '#title' => t('List Title'),
       '#list_items' => [
-        ['title' => t('Test 1'), 'author' => t('Author 1'), 'description' => t('Description 1')],
-        ['title' => t('Test 2'), 'author' => t('Author 2'), 'description' => t('Description 2')],
+        [
+          'title' => t('Test 1'),
+          'author' => t('Author 1'),
+          'description' => t('Description 1'),
+        ],
+        [
+          'title' => t('Test 2'),
+          'author' => t('Author 2'),
+          'description' => t('Description 2'),
+        ],
       ],
     ];
 
   }
+
+  function type() {
+    $build =  [
+      '#theme' => 'custom_list',
+      '#title' => t('List Title'),
+      '#list_items' => [
+        [
+          'title' => t('Test 1'),
+          'author' => t('Author 1'),
+          'description' => t('Description 1'),
+        ],
+        [
+          'title' => t('Test 2'),
+          'author' => t('Author 2'),
+          'description' => t('Description 2'),
+        ],
+      ],
+    ];
+
+    return $build;
+  }
+
 }
