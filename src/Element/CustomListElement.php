9c8caa9eb2142a4e49b9e48d30c645ead6bf0402
NotifyOne <anton220812@gmail.com>
2019-09-25 12:59:02 +0300

    Rename filename and class. Close #6

--- src/Element/CustomListElement.php
+++ src/Element/CustomListElement.php
@@ -39,7 +39,7 @@ use Drupal\Core\Render\Element\RenderElement;
  *
  * @RenderElement("custom_list")
  */
-class customListElement extends RenderElement {
+class CustomListElement extends RenderElement {
 
   /**
    * {@inheritdoc}
